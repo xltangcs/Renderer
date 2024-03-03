fe17e6627e3e9c513d1a69294864690bc975a6e2
xltangcs <xltangcs@outlook.com>
2024-03-03 23:46:36 +0800

    Can Rasterize Triangle

--- Renderer/src/App/Renderer.cpp
+++ Renderer/src/App/Renderer.cpp
@@ -18,24 +18,19 @@ namespace Utils
 
 Renderer::Renderer()
 {
-	rendermode = Point;
+	rendermode = Triangle;
+
+	m_FinalImage = std::make_shared<Image>(m_Width, m_Height);
 }
 
 void Renderer::OnResize(unsigned int width, unsigned int height)
 {
-	if (m_FinalImage)
-	{
-		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
-			return;
-		m_FinalImage->Resize(width, height);
-	}
-	else
-	{
-		m_FinalImage = std::make_shared<Image>(width, height);
-	}
-	delete[] m_ImageData;
-	m_ImageData = new uint32_t[width * height];
-	memset(m_ImageData, 0, width * height * sizeof(uint32_t));
+	if (m_Width == width && m_Height == height) return;
+
+	m_Width = width; m_Height = height;
+	m_FinalImage->Resize(m_Width, m_Height);
+
+	ResetData();
 }
 
 
@@ -53,11 +48,11 @@ void Renderer::Render(Camera& camera, Scene& scene)
 	glm::vec3 color = glm::vec3(1.0f);
 	switch (rendermode)
 	{
-	case Point: RenderPoint(color);
+	case Point: RasterizePoint(color);
 		break;
-	case Line: RenderLine(color);
+	case Line: RasterizeLine(color);
 		break;
-	case Triangle:
+	case Triangle: RasterizeTriangle(color);
 		break;
 	default:
 		break;
@@ -66,7 +61,46 @@ void Renderer::Render(Camera& camera, Scene& scene)
 	m_FinalImage->SetData(m_ImageData);
 }
 
-void Renderer::RenderPoint(glm::vec3 color)
+void Renderer::ResetData()
+{
+	delete[] m_ImageData;
+	delete[] z_buffer;
+	m_ImageData = new uint32_t[m_Width * m_Height];
+	z_buffer = new uint8_t[m_Width * m_Height];
+	memset(m_ImageData, 0, m_Width * m_Height * sizeof(uint32_t));
+	memset(z_buffer, 0, m_Width * m_Height * sizeof(uint8_t));
+}
+
+bool Renderer::InsideTriangle(glm::vec2 p, std::vector<glm::vec3> t)
+{
+	glm::vec3 P = glm::vec3(p, 0.0f);
+	glm::vec3 AC = t[2] - t[0];
+	glm::vec3 CB = t[1] - t[2];
+	glm::vec3 BA = t[0] - t[1];
+	glm::vec3 AP = P - t[0];
+	glm::vec3 BP = P - t[1];
+	glm::vec3 CP = P - t[2];
+
+	//if cross product in the same direction ,its inside the triangle
+	if (glm::dot(glm::cross(AP, AC), glm::cross(BP, BA)) > 0.0f &&
+		glm::dot(glm::cross(BP, BA), glm::cross(CP, CB)) > 0.0f &&
+		glm::dot(glm::cross(CP, CB), glm::cross(AP, AC)) > 0.0f)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+std::tuple<float, float, float> Renderer::computeBarycentric2D(glm::vec2 p, std::vector<glm::vec3> v)
+{
+	float c1 = (p.x * (v[1].y - v[2].y) + (v[2].x - v[1].x) * p.y + v[1].x * v[2].y - v[2].x * v[1].y) / (v[0].x * (v[1].y - v[2].y) + (v[2].x - v[1].x) * v[0].y + v[1].x * v[2].y - v[2].x * v[1].y);
+	float c2 = (p.x * (v[2].y - v[0].y) + (v[0].x - v[2].x) * p.y + v[2].x * v[0].y - v[0].x * v[2].y) / (v[1].x * (v[2].y - v[0].y) + (v[0].x - v[2].x) * v[1].y + v[2].x * v[0].y - v[0].x * v[2].y);
+	float c3 = (p.x * (v[0].y - v[1].y) + (v[1].x - v[0].x) * p.y + v[0].x * v[1].y - v[1].x * v[0].y) / (v[2].x * (v[0].y - v[1].y) + (v[1].x - v[0].x) * v[2].y + v[0].x * v[1].y - v[1].x * v[0].y);
+	return { c1,c2,c3 };
+}
+
+void Renderer::RasterizePoint(glm::vec3 color)
 {
 	auto model = m_Scene->m_Models[0];
 
@@ -83,12 +117,12 @@ void Renderer::RenderPoint(glm::vec3 color)
 			int y0 = -vv0.y / vv0.w * m_FinalImage->GetHeight() / 2.0 + m_FinalImage->GetHeight() / 2.0;
 
 			if (x0 >= 0 && x0 < m_FinalImage->GetWidth() && y0 >= 0 && y0 < m_FinalImage->GetHeight())
-				m_ImageData[x0 + y0 * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(glm::vec4(color, 1.0f));
+				DrawPoint(glm::vec2(x0, y0), color);
 		}
 	}
 }
 
-void Renderer::RenderLine(glm::vec3 color)
+void Renderer::RasterizeLine(glm::vec3 color)
 {
 	auto model = m_Scene->m_Models[0];
 
@@ -116,6 +150,34 @@ void Renderer::RenderLine(glm::vec3 color)
 	}
 }
 
+void Renderer::RasterizeTriangle(glm::vec3 color)
+{
+	auto model = m_Scene->m_Models[0];
+	for (int i = 0; i < model->nFaces(); i++)
+	{
+		std::vector<int> facei = model->GetFace(i);
+		std::vector<glm::vec3> screenCoords(3);
+		std::vector<glm::vec3> modelCoords(3);
+		for (int i = 0; i < 3; i++)
+		{
+			modelCoords[i] = model->GetVert(facei[i]);
+			glm::vec4 clipCoords = m_Camera->GetProjection() * m_Camera->GetView() * glm::vec4(modelCoords[i], 1.0f);
+
+			screenCoords[i].x = clipCoords.x / clipCoords.w * m_FinalImage->GetWidth() / 2.0 + m_FinalImage->GetWidth() / 2.0;
+			screenCoords[i].y = -clipCoords.y / clipCoords.w * m_FinalImage->GetHeight() / 2.0 + m_FinalImage->GetHeight() / 2.0;
+			screenCoords[i].z = clipCoords.z / clipCoords.w;
+		}
+
+		DrawTriangle(screenCoords, color);
+	}
+}
+
+void Renderer::DrawPoint(glm::vec2 p, glm::vec3 color)
+{
+	int index = p.x + p.y * m_FinalImage->GetWidth();
+	m_ImageData[index] = Utils::ConvertToRGBA(glm::vec4(color, 1.0f));;
+}
+
 void Renderer::DrawLine(glm::vec2 p1, glm::vec2 p2, glm::vec3 color)
 {
 	int x0 = p1.x, y0 = p1.y;
@@ -127,9 +189,9 @@ void Renderer::DrawLine(glm::vec2 p1, glm::vec2 p2, glm::vec3 color)
 	int sy = (y0 < y1) ? 1 : -1;
 	int err = dx - dy;
 
-	while (x0 != x1 || y0 != y1) 
+	while (x0 != x1 || y0 != y1)
 	{
-		m_ImageData[x0 + y0 * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(glm::vec4(color, 1.0f));
+		DrawPoint(glm::vec2(x0, y0), color);
 		int err2 = 2 * err;
 		if (err2 > -dy) {
 			err -= dy;
@@ -140,6 +202,52 @@ void Renderer::DrawLine(glm::vec2 p1, glm::vec2 p2, glm::vec3 color)
 			y0 += sy;
 		}
 	}
-	m_ImageData[x1 + y1 * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(glm::vec4(color, 1.0f));
+	DrawPoint(glm::vec2(x1, y1), color);
+}
+
+void Renderer::DrawTriangle(std::vector<glm::vec3> t, glm::vec3 color)
+{
+	bool IsUseSurperSampling = true;
+
+	//Sample Offset
+	std::vector<glm::vec3> SampleOffset = {
+		{0.25f,0.25f,0},
+		{0.25f,0.75f,0},
+		{0.75f,0.25f,0},
+		{0.75f,0.75f,0}
+	};
+
+	//First: Ceater bounding box
+	float minX = std::max(std::min(t[2].x, std::min(t[0].x, t[1].x)), 0.0f);
+	float maxX = std::min(std::max(t[2].x, std::max(t[0].x, t[1].x)), (float)m_Width);
+
+	float minY = std::max(std::min(t[2].y, std::min(t[0].y, t[1].y)), 0.0f);
+	float maxY = std::min(std::max(t[2].y, std::max(t[0].y, t[1].y)), (float)m_Height);
+
+	minX = (int)std::floor(minX); 
+	maxX = (int)std::ceil(maxX); 
+	minY = (int)std::floor(minY); 
+	maxY = (int)std::ceil(maxY);
+
+	for (int i = minX; i < maxX; i++)
+	{
+		for (int j = minY; j < maxY; j++)
+		{
+			int Index = i + j * m_FinalImage->GetWidth();
+			if (InsideTriangle(glm::vec2(i + 0.5f, j + 0.5f), t))
+			{
+
+				auto [alpha, beta, gamma] = computeBarycentric2D(glm::vec2(i + 0.5f, j + 0.5f), t);
+				float z_interpolated = alpha * t[0].z + beta * t[1].z + gamma * t[2].z;
+
+				// check zbuff
+				if (z_interpolated > z_buffer[Index])
+				{
+					z_buffer[Index] = z_interpolated;
+					DrawPoint(glm::vec2(i, j), color);
+				}
+			}
+		}
+	}
 }
 
--- Renderer/src/App/Renderer.h
+++ Renderer/src/App/Renderer.h
@@ -27,19 +27,31 @@ public:
 	void OnResize(unsigned int width, unsigned int height);
 	void Render(Camera& camera, Scene& scene);
 
-	void RenderPoint(glm::vec3 color);
-	void RenderLine(glm::vec3 color);
-
-	void DrawLine(glm::vec2 p1, glm::vec2 p2, glm::vec3 color);
-
 	std::shared_ptr<Image> GetImage() const { return  m_FinalImage; }
+private:
 
+	void ResetData();
+
+	bool InsideTriangle(glm::vec2 p, std::vector<glm::vec3> t);
+	std::tuple<float, float, float> computeBarycentric2D(glm::vec2 p, std::vector<glm::vec3> t);
+
+
+	void RasterizePoint(glm::vec3 color);
+	void RasterizeLine(glm::vec3 color);
+	void RasterizeTriangle(glm::vec3 color);
+
+	void DrawPoint(glm::vec2 p, glm::vec3 color);
+	void DrawLine(glm::vec2 p0, glm::vec2 p1, glm::vec3 color);
+	void DrawTriangle(glm::vec2 p0, glm::vec2 p1, glm::vec2 p2, glm::vec3 color);
+	void DrawTriangle(std::vector<glm::vec3> t, glm::vec3 color);
 
 private:
+	uint32_t m_Width = 10, m_Height = 10;
 	Camera* m_Camera = nullptr;
 	Scene* m_Scene = nullptr;
 	std::shared_ptr<Image> m_FinalImage = nullptr;
 
 	uint32_t* m_ImageData = nullptr;
+	uint8_t* z_buffer = nullptr;
 	bool reset = false;
 };
